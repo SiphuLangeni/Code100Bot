5577ccf6645cb7fe0f630306399974815acb20cc
Siphu Langeni, MS <szlangeni@gmail.com>
2021-01-17 14:34:01 -0500

    Update logs

--- code100bot.py
+++ code100bot.py
@@ -2,7 +2,6 @@ from os import environ
 from datetime import datetime as dt
 from datetime import timedelta
 from time import sleep
-import tweepy
 from tweepy import OAuthHandler, Stream, API
 from tweepy.streaming import StreamListener
 import logging
@@ -25,7 +24,7 @@ keywords = ['data science', 'datascience', 'machine learning', \
 class LikesListener(StreamListener):
 
     
-    def __init__(self, api, keywords=keywords, max_likes=2, update_interval=1, delta=1):
+    def __init__(self, api, keywords=keywords, max_likes=1, update_interval=1, delta=1):
         
         self.api = api
         self.keywords = keywords
@@ -60,11 +59,13 @@ class LikesListener(StreamListener):
                     tweet.favorite()
                     self.num_likes += 1
                     if self.num_likes % self.update_interval == 0:
-                        logger.info(f'Liked {self.num_likes} tweets in {diff.seconds / 3600:.2f} \
-                            hours ({dt.strftime(now, "%H:%M")})')
+                        logger.info(
+                            f'\nLiked {self.num_likes} {self.plural(self.num_likes)} '
+                            f'at {dt.strftime(now, "%H:%M")}\n'
+                        )
 
                 except Exception as e:
-                    logger.error(e)
+                    logger.error(f'\n{e}\n')
 
 
                 if self.num_likes == self.max_likes:
@@ -74,16 +75,34 @@ class LikesListener(StreamListener):
                     pause_time_str = dt.strftime(now, '%H:%M')
                     pause_date_str = dt.strftime(now, '%b %d')
                     sleep_time = 86_400 - diff.seconds
+                    
+                    resume_at = now + timedelta(seconds=sleep_time)
+                    resume_time_str = dt.strftime(resume_at, '%H:%M')
+                    resume_date_str = dt.strftime(resume_at, '%b %d')
 
-                    logger.info(f'Started at {start_time_str} on {start_date_str}')
-                    logger.info(f'Paused at {pause_time_str} on {pause_date_str}')
-                    logger.info(f'{self.num_likes} tweets liked')
-                    logger.info(f'Sleeping for {sleep_time / 3600:.2f} hours')
+
+                    logger.info(
+                        f'\nStarted at {start_time_str} on {start_date_str}\n'
+                        f'Paused at {pause_time_str} on {pause_date_str}\n'
+                        f'Liked {self.num_likes} {self.plural(self.num_likes)}\n'
+                        f'Sleeping for {sleep_time / 3600:.2f} hours\n'
+                        f'Code100Bot will resume at {resume_time_str} on {resume_date_str}\n'
+                    )
                     sleep(sleep_time)
                     self.start_time = now
                     self.num_likes = 0
                 
-            
+
+    def plural(self, num_tweets):
+        
+        if self.num_likes == 1:
+            plural = 'tweet'
+        else:
+            plural = 'tweets'
+        
+        return plural
+
+
     def on_error(self, status_code):
         if status_code == 420:
             return False
@@ -96,8 +115,10 @@ def twitter_auth():
     Returns auth
     '''
 
-    auth = OAuthHandler(environ.get('CONSUMER_KEY'), environ.get('CONSUMER_SECRET'))
-    auth.set_access_token(environ.get('ACCESS_TOKEN'), environ.get('ACCESS_TOKEN_SECRET'))
+    auth = OAuthHandler(environ.get('CONSUMER_KEY'), \
+        environ.get('CONSUMER_SECRET'))
+    auth.set_access_token(environ.get('ACCESS_TOKEN'), \
+        environ.get('ACCESS_TOKEN_SECRET'))
     
     api = API(
         auth,
@@ -105,26 +126,17 @@ def twitter_auth():
         wait_on_rate_limit_notify=True
     )
 
-    logger.info('Authenticating Code100Bot')
     api.verify_credentials()
         
     return api
         
-    # except tweepy.error.TweepError:
-    #     print('Unable to connect')
-    #     return False
-
-        
-    #     #  logger.error(e, exc_info=False)
-    #     # print(e.api_code)
-
-    
-
 
 def like_tweets(api, hashtag_list):
 
     likes_listener = LikesListener(api)
+    logger.info('\nCode100Bot ready\n')
     stream = Stream(api.auth, likes_listener, tweet_mode='extended')
+    logger.info('\nSearching tweets...\n')
     stream.filter(track=hashtag_list, languages=['en'])
 
 
@@ -134,7 +146,8 @@ if __name__ == "__main__":
     
     try:
         like_tweets(api, hashtag_list)
+    
     except Exception as e:
-        print(e)
+        logger.error(f'\n{e}\n')
         
     
